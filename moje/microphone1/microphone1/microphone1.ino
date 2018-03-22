4028d7ae04d20129b47a893762c3c8acb53146dc
Unknown <j.j.nastycrue@gmail.com>
2018-03-22 22:33:54 +0100

    Adding new pin, HW works, prototype works

--- moje/microphone1/microphone1/microphone1.ino
+++ moje/microphone1/microphone1/microphone1.ino
@@ -1,7 +1,7 @@
 #include <TimerOne.h>
 
 const u8  led_1 = 13;
-
+const u8  relay_pin = 7;
 /*
    To get fs=20kHz we need T=1/f => T=0.00005s => T=50us
    @16MHz clock we've got a tick every 0.0625 us. This gives us
@@ -10,16 +10,20 @@ const u8  led_1 = 13;
 
 const u16 interval = 50; //50us->800ticks, 100->1600ticks
 
-volatile bool light1  = HIGH;
+volatile bool light1  = LOW;
 volatile u16 sample  = 0;
 volatile bool new_sample  = false;
 
 void setup()
 {
   pinMode(led_1, OUTPUT);
+  pinMode(relay_pin, OUTPUT);
   pinMode(A0, INPUT);
-
-  //Serial.begin(9600);
+  
+  digitalWrite(led_1, light1);
+  digitalWrite(relay_pin, light1);
+      
+  Serial.begin(9600);
 
   //timer
   Timer1.initialize(interval);
@@ -44,6 +48,8 @@ void loop()
     {
       light1 = !light1;
       digitalWrite(led_1, light1);
+      digitalWrite(relay_pin, light1);
+      Serial.println(sample);
     }
   }
 
